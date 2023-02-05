2f79ae2212cc0bcb7da275236afa9799729a3c14
CosmoLuna <annilaust@gmail.com>
2023-02-05 20:42:22 +0500

    hw added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # print('Hello')
 # print("Здесь будут домашние работы")
+import re
 
 # Методы строк
 # Задача 1
@@ -49,17 +50,74 @@
 # print((lambda a, b, c: a * b * c)(2, 5, 5))
 
 # Задача 3
-test = [{'name': 'Jennifer', 'final': 95},
-        {'name': 'David', 'final': 92},
-        {'name': 'Nikolas', 'final': 98}]
-print(test)
-res = sorted(test, key=lambda item: item['name'])
-print(res)
-res = sorted(test, reverse=True, key=lambda item: item['final'])
-print(res)
+# test = [{'name': 'Jennifer', 'final': 95},
+#         {'name': 'David', 'final': 92},
+#         {'name': 'Nikolas', 'final': 98}]
+# print(test)
+# res = sorted(test, key=lambda item: item['name'])
+# print(res)
+# res = sorted(test, reverse=True, key=lambda item: item['final'])
+# print(res)
+#
+# # Задача 4
+# res = max(test, key=lambda item: item['final'])
+# print(res)
+# res = min(test, key=lambda item: item['final'])
+# print(res)
 
-# Задача 4
-res = max(test, key=lambda item: item['final'])
-print(res)
-res = min(test, key=lambda item: item['final'])
-print(res)
+# Декораторы
+
+# def func(fn):
+#     def wrap(*nums):
+#         fn(*nums)
+#         print("Среднее арифметическое чисел", *nums, "=", sum(nums) / len(nums))
+#     return wrap
+#
+#
+# @func
+# def summa(*nums):
+#     print("Сумма чисел", *nums, "=", sum(nums))
+#
+#
+# summa(2, 3, 3, 4)
+
+
+# методы строк и re
+# Задача 1
+import re
+
+# def password_ok(pas):
+#     return re.findall(r'^[a-z\d@_-]{6,18}$', pas, re.IGNORECASE)
+#
+#
+# passw = input("Введите пароль: ")
+# print("Пароль", password_ok(passw), "сохранен")
+
+# Задача 2
+# my_date = 'В июне 2021 года, 02/06/2021, 05/06/2021, 14/06/2021, были зафиксированы максимумы ежемесячных осадков.'
+# reg = r'[\d]{2}/\d{2}/\d{4}'
+# print(re.findall(reg, my_date))
+
+# re и рекурсия
+# Задача 1
+# def valid_num(tel):
+#     reg = r''
+#     return re.findall(reg, tel)
+#
+#
+# tel_num = input("Введите номер телефона: ")
+# print(valid_num(tel_num))
+
+# Задача 2
+# def neg_val(lst):
+#     if len(lst) == 0:
+#         return 0
+#     else:
+#         count = neg_val(lst[1:])
+#         if lst[0] < 0:
+#             count += 1
+#         return count
+#
+#
+# l1 = [3, -5, 11, 2, -7, 8, -2]
+# print(neg_val(l1))
